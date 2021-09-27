dd4628e3fd5cabebc4516b1f404b37b2b1dbc682
falconcloud2021 <bondarchuck.yaroslaw@gmail.com>
2021-09-27 09:38:50 +0300

    config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'yaroslavloc');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATION', 'utf8_general_ci');
+define('DB_PREFIX', '');
