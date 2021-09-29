54b41729a697cdea23275f83fd8d8924c551271f
falconcloud2021 <bondarchuck.yaroslaw@gmail.com>
2021-09-29 10:12:43 +0300

    resources/cache/f1b0bf1a0056feef5e1ed47a5ecfc1117af6b707.php

---
+++ resources/cache/f1b0bf1a0056feef5e1ed47a5ecfc1117af6b707.php
@@ -0,0 +1,112 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+  <ul class="nav">
+    <li class="nav-item nav-profile">
+      <a href="/admin/profile" class="nav-link">
+        <div class="nav-profile-image">
+          <img src="/resources/media/images/<?php echo e($user->ava); ?>" name="ava" alt="ava">
+          <span class="login-status online"></span>
+          <!--change to offline or busy as needed-->
+        </div>
+        <div class="nav-profile-text d-flex flex-column">
+          <span class="font-weight-bold mb-2"> <?php echo e($user->first_name); ?> <?php echo e($user->second_name); ?> </span>
+          <span class="text-secondary text-small"> <?php echo e($user->g_name); ?> </span>
+        </div>
+        <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
+      </a>
+    </li>
+    <li class="nav-item"> 
+      <a class="nav-link" href="/admin/dashboard">
+        <span class="menu-title">Dashboard</span>
+        <i class="mdi mdi-home menu-icon"></i>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/admin/products">
+        <span class="menu-title">Products</span>
+        <i class="mdi mdi-barcode-scan menu-icon"></i>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/admin/users_all">
+        <span class="menu-title">Users</span>
+        <i class="mdi mdi-account-multiple menu-icon"></i>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/admin/profile">
+        <span class="menu-title">My Profile</span>
+        <i class="mdi mdi-account-circle menu-icon"></i>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/admin/charts">
+        <span class="menu-title">Charts</span>
+        <i class="mdi mdi-chart-bar menu-icon"></i>
+      </a>
+    </li>  
+    <li class="nav-item">
+      <a class="nav-link" href="/admin/tables">
+        <span class="menu-title">Tables</span>
+        <i class="mdi mdi-table-large menu-icon"></i>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/admin/forms">
+        <span class="menu-title">Forms</span>
+        <i class="mdi mdi-format-list-bulleted menu-icon"></i>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="pages/icons/mdi.html">
+        <span class="menu-title">Icons</span>
+        <i class="mdi mdi-contacts menu-icon"></i>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+        <span class="menu-title">Basic UI Elements</span>
+        <i class="menu-arrow"></i>
+        <i class="mdi mdi-crosshairs-gps menu-icon"></i>
+      </a>
+      <div class="collapse" id="ui-basic">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
+        </ul>
+      </div>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="collapse" href="#general-pages" aria-expanded="false" aria-controls="general-pages">
+        <span class="menu-title">Sample Pages</span>
+        <i class="menu-arrow"></i>
+        <i class="mdi mdi-medical-bag menu-icon"></i>
+      </a>
+      <div class="collapse" id="general-pages">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html"> Blank Page </a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
+          <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
+        </ul>
+      </div>
+    </li>
+    <li class="nav-item sidebar-actions">
+      <span class="nav-link">
+        <div class="border-bottom">
+          <h6 class="font-weight-normal mb-3">Projects</h6>
+        </div>
+        <button class="btn btn-block btn-lg btn-gradient-primary mt-4">+ Add a project</button>
+        <div class="mt-4">
+          <div class="border-bottom">
+            <p class="text-secondary">Categories</p>
+          </div>
+          <ul class="gradient-bullet-list mt-4">
+            <li>Black</li>
+            <li>White </li>
+          </ul>
+        </div>
+      </span>
+    </li>
+  </ul>
+</nav><?php /**PATH W:\domains\flightproject\app\Views/admin/partials/sidebar.blade.php ENDPATH**/ ?>
